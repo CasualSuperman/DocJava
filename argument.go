2bfa7ee34e2918e7953d48f506ebf9839320bd16
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-12 12:34:50 -0400

    Started work on writing constructor parsing.

---
+++ argument.go
@@ -0,0 +1,6 @@
+package main
+
+type Argument struct {
+    argName string
+    argType Type
+}
--- constructor.go
+++ constructor.go
@@ -1,3 +1,22 @@
 package main
 
-type Constructor struct{}
+
+type Constructor struct {
+    constructorModifiers Maskable // Optional
+    constructorDeclarator conDeclarator
+    // throws
+    throws string // Optional
+    // {
+    // Body
+    // }
+}
+
+type conDeclarator struct {
+    typeParameters string // Optional
+    typeTypes Type
+    // (
+    formalParameterList []Argument
+    // )
+}
+
+var cn_public, cn_private, cn_protected Mask
