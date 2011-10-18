4fce22c1e793da36f22556ce436b1cc3c3cc8dd2
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-17 22:17:47 -0400

    Argument parsing is a go.
    Will probably break if used with a complex type, such as a generic
    or nested generics.

--- argument.go
+++ argument.go
@@ -1,12 +1,28 @@
 package main
 
+import (
+	"regexp"
+	"strings"
+)
+
 type Argument struct {
 	argName string
 	argType Type
 }
 
 func NewArgList(s string) []Argument {
-	return []Argument{}
+	s = regexp.MustCompile("</?a[^>]*>").ReplaceAllString(s, "")
+	args := strings.Split(s, " ")
+	result := []Argument{}
+	for i := 0; i < len(args); i++ {
+		result = append(result, parse(args[i]))
+	}
+	return result
+}
+
+func parse(s string) Argument {
+	halves := strings.Split(s, "&nbsp;")
+	return Argument{halves[0], NewType(halves[1])}
 }
 
 func (a Argument) String() (s string) {
