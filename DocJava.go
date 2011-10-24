c7a3d42c19c20b8f20f05adca145fb30d6bfb157
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-23 23:20:10 -0400

    SplitClass should be mostly done.

--- DocJava.go
+++ DocJava.go
@@ -31,34 +31,57 @@ func SplitClass(html string) (result []string) {
 		"", // Field
 		"", // Constructor
 		""} // Method
-	menu := strings.SplitN(html, "<ul class=\"subNavList\">\n<li>Detail:&nbsp;</li>\n", 2)[1]
+	menu := strings.SplitN(html, "<ul class=\"subNavList\">\n<li>Summary:&nbsp;</li>\n", 2)[1]
 	list := strings.SplitN(menu, "</ul>", 2)[0]
 	temp := regexp.MustCompile("<li><a href=[^>]+>([^<]+)</a>").FindAllStringSubmatch(list, -1)
 	sections := []string{"START"}
 	for _, val := range temp {
 		sections = append(sections, val[1])
 	}
-	section := map[string]int{
-		"Field":  3,
-		"Constr": 4,
-		"Method": 5}
+	section := map[string]detail{
+		"Nested":    detail{1, false},
+		"Interface": detail{2, false},
+		"Field":     detail{3, true},
+		"Constr":    detail{4, true},
+		"Method":    detail{5, true}}
 	sections = append(sections, "END")
 	for i, val := range sections {
 		if val == "START" || val == "END" {
 			continue
 		}
-		result[section[val]] = splitSection(html, sections[i-1], val)
+		index := section[val].Index
+		if section[val].Detail {
+			result[index] = splitDetailSection(html, val, sections[i+1])
+		} else {
+			result[index] = splitSummarySection(html, val, sections[i+1])
+		}
 	}
+	//return sections
 	return result
 }
 
-func splitSection(data, first, second string) (result string) {
+type detail struct {
+	Index  int
+	Detail bool
+}
+
+func splitSummarySection(data, start, end string) (result string) {
+	delimeters := map[string]string{
+		"START":  "<!-- ======== START OF CLASS DATA ======== -->",
+		"Nested": "<!-- ======== NESTED CLASS SUMMARY ======== -->",
+		"Field":  "<!-- =========== FIELD SUMMARY =========== -->",
+		"Constr": "<!-- ======== CONSTRUCTOR SUMMARY ======== -->",
+		"Method": "<!-- ========== METHOD SUMMARY =========== -->",
+		"END":    "<div class=\"details\">\n<ul class=\"blockList\">\n<li class=\"blockList\">"}
+	return strings.Split(strings.Split(data, delimeters[start])[1], delimeters[end])[0]
+}
+
+func splitDetailSection(data, start, end string) string {
 	delimeters := map[string]string{
 		"START":  "<div class=\"details\">\n<ul class=\"blockList\">\n<li class=\"blockList\">",
 		"Field":  "<!-- ============ FIELD DETAIL =========== -->",
 		"Constr": "<!-- ========= CONSTRUCTOR DETAIL ======== -->",
 		"Method": "<!-- ============ METHOD DETAIL ========== -->",
 		"END":    "<!-- ========= END OF CLASS DATA ========= -->"}
-	result = strings.Split(strings.Split(data, delimeters[first])[1], delimeters[second])[0]
-	return
+	return strings.Split(strings.Split(data, delimeters[start])[1], delimeters[end])[0]
 }
